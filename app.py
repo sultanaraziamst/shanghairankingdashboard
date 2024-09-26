94f2cfe0b18f5ae0e4c45ff51eba297c1562b796
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-09-26 11:33:36 +0600

    updates

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ st.plotly_chart(fig,use_container_width=True)
 
 with col5:
     top_100_universities = df.nlargest(100, 'N&S')
-    fig = px.scatter(df, x = "N&S", y="PCP", color="Hici", labels={"Not significant": "PCP{}"},
-                title="The score representing the number of research articles published in Nature and Science between 2019 and 2023.", hover_data=["Alumni"],
+    fig = px.scatter(df, x = "N&S", y="PCP", color="N&S", labels={"Not significant": "PCP{}"},
+                title="The score representing the number of research articles published in Nature and Science between 2019 and 2023..", hover_data=["Alumni"],
                 template="gridon", height=500)
 st.plotly_chart(fig,use_container_width=True)
 
